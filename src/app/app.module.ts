8bccc7671cee8c73cd9b88aa607f2e725a96c869
RobertTheDev <robertthedev@gmail.com>
2023-05-25 23:58:04 +0100

    Updated Angular App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventDetailComponent } from './event/event-detail/event-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EventDetailComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EventDetailComponent } from './event-detail/event-detail.component';
 import { EventCardComponent } from './event-card/event-card.component';
 
-
-
 @NgModule({
-  declarations: [
-    EventDetailComponent,
-    EventCardComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [EventCardComponent],
+  imports: [CommonModule],
 })
-export class EventModule { }
+export class EventModule {}
