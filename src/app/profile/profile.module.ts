fda2cb076c5c3237d8e1d25de8937bb5a46a8a31
RobertTheDev <robertthedev@gmail.com>
2023-05-26 17:53:48 +0100

    Exported Profile Views

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileViewComponent } from './views/profile-view/profile-view.component';
+import { AccountSettingsViewComponent } from './views/account-settings-view/account-settings-view.component';
+import { EditProfileViewComponent } from './views/edit-profile-view/edit-profile-view.component';
+import { SavedEventsViewComponent } from './views/saved-events-view/saved-events-view.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileViewComponent,
+    AccountSettingsViewComponent,
+    EditProfileViewComponent,
+    SavedEventsViewComponent,
+  ],
   imports: [CommonModule],
+  exports: [
+    ProfileViewComponent,
+    AccountSettingsViewComponent,
+    EditProfileViewComponent,
+    SavedEventsViewComponent,
+  ],
 })
 export class ProfileModule {}
