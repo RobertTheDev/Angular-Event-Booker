d1aec7921cbd5b29a462b3130fa747839401a678
RobertTheDev <robertthedev@gmail.com>
2023-05-26 21:37:03 +0100

    Created Create Event Form Component

---
+++ src/app/event/components/forms/create-event-form/create-event-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-event-form',
+  templateUrl: './create-event-form.component.html',
+  styleUrls: ['./create-event-form.component.sass']
+})
+export class CreateEventFormComponent {
+
+}
