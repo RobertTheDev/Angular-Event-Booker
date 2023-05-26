9a977ad38d54df67166727659da9d84fdf0c2f4e
RobertTheDev <robertthedev@gmail.com>
2023-05-26 05:16:07 +0100

    Created Event Detail Component

--- src/app/event/event-detail/event-detail.component.ts
+++ src/app/event/event-detail/event-detail.component.ts
@@ -1,41 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../event.service';
+import Event from 'src/types/Event';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-detail',
   templateUrl: './event-detail.component.html',
   styleUrls: ['./event-detail.component.sass'],
 })
-export class EventDetailComponent {
-  event = {
-    id: '1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    name: 'Tribute to Hans Zimmer & John Williams by Moonlight: Edinburgh Late Concert',
-    headline:
-      "An immersive tribute to Hans Zimmer and John William's most iconic music with a live chamber orchestra in a moon-lit setting.",
-    refundPolicy: null,
-    location: {
-      id: '',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      address: '',
-      town: '',
-      region: '',
-      country: '',
-    },
-    about: null,
-    ticketType: 'E-Ticket',
-    image: {
-      createdAt: new Date(),
-      updatedAt: null,
-      url: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F465117129%2F61565826027%2F1%2Foriginal.20230308-134732?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C23%2C2048%2C1024&s=4268e68912849bf25d2355a4cc86a1fb',
-      alt: '',
-    },
-    dateOfEvent: new Date(),
-    tags: ['Cat', 'Dog'],
-    duration: {
-      length: 6,
-      unit: 'hours',
-    },
-  };
+export class EventDetailComponent implements OnInit {
+  event: Event | undefined = undefined;
+
+  constructor(
+    private eventService: EventService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      this.getEvent(id);
+    });
+  }
+
+  getEvent(id: string | null) {
+    if (id) {
+      this.event = this.eventService.getEventById(id);
+    }
+  }
 }
