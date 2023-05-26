10818ff410076a582072a871ab3dbbcfffac0db4
RobertTheDev <robertthedev@gmail.com>
2023-05-26 05:16:21 +0100

    Updated Angular App

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventCardComponent } from './event-card/event-card.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventCardListComponent } from './event-card-list/event-card-list.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [EventCardComponent],
-  imports: [CommonModule],
+  declarations: [
+    EventCardComponent,
+    EventCardListComponent,
+    EventDetailComponent,
+  ],
+  imports: [CommonModule, AppRoutingModule],
+  exports: [EventCardComponent, EventCardListComponent, EventDetailComponent],
 })
 export class EventModule {}
