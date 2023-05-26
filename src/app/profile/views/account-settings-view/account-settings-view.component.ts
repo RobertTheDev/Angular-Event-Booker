4da9d8bcb10d86071213bccd5c5c93d26eb9a30a
RobertTheDev <robertthedev@gmail.com>
2023-05-26 17:53:36 +0100

    Created Account Settings View

---
+++ src/app/profile/views/account-settings-view/account-settings-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-settings-view',
+  templateUrl: './account-settings-view.component.html',
+  styleUrls: ['./account-settings-view.component.sass']
+})
+export class AccountSettingsViewComponent {
+
+}
