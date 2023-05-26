6e2b6aace4599568e79060bfa424f04848819a10
RobertTheDev <robertthedev@gmail.com>
2023-05-26 07:59:54 +0100

    Added Views To Orgnaisation Module

--- src/app/organisation/organisation.module.ts
+++ src/app/organisation/organisation.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { OrganisationsViewComponent } from './views/organisations-view/organisations-view.component';
+import { OrganisationViewComponent } from './views/organisation-view/organisation-view.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [OrganisationsViewComponent, OrganisationViewComponent],
+  imports: [CommonModule],
 })
-export class OrganisationModule { }
+export class OrganisationModule {}
