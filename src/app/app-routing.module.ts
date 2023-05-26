cc41e8169ea909982a3183fec1f594ca36fb33f6
RobertTheDev <robertthedev@gmail.com>
2023-05-26 08:00:05 +0100

    Added Organisation Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,12 @@ import { SignUpViewComponent } from './auth/views/sign-up-view/sign-up-view.comp
 import { LoginViewComponent } from './auth/views/login-view/login-view.component';
 import { EventViewComponent } from './event/views/event-view/event-view.component';
 import { EventsViewComponent } from './event/views/events-view/events-view.component';
+import { AboutViewComponent } from './info/views/about-view/about-view.component';
+import { PrivacyPolicyViewComponent } from './info/views/privacy-policy-view/privacy-policy-view.component';
+import { TermsViewComponent } from './info/views/terms-view/terms-view.component';
+import { AccessibilityStatementViewComponent } from './info/views/accessibility-statement-view/accessibility-statement-view.component';
+import { OrganisationViewComponent } from './organisation/views/organisation-view/organisation-view.component';
+import { OrganisationsViewComponent } from './organisation/views/organisations-view/organisations-view.component';
 
 const routes: Routes = [
   {
@@ -35,6 +41,40 @@ const routes: Routes = [
       {
         path: '/:id',
         component: EventViewComponent,
+      },
+    ],
+  },
+  {
+    path: 'info',
+    children: [
+      {
+        path: 'about',
+        component: AboutViewComponent,
+      },
+      {
+        path: 'privacy-policy',
+        component: PrivacyPolicyViewComponent,
+      },
+      {
+        path: 'terms-and-conditions',
+        component: TermsViewComponent,
+      },
+      {
+        path: 'accessibility-statement',
+        component: AccessibilityStatementViewComponent,
+      },
+    ],
+  },
+  {
+    path: 'organisations',
+    children: [
+      {
+        path: '',
+        component: OrganisationsViewComponent,
+      },
+      {
+        path: '/:id',
+        component: OrganisationViewComponent,
       },
     ],
   },
