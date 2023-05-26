a6d37607ac674bbb0887be215bb4857b4cd4e274
RobertTheDev <robertthedev@gmail.com>
2023-05-26 21:33:51 +0100

    Built Sign Up Form

--- src/app/auth/components/forms/sign-up-form/sign-up-form.component.ts
+++ src/app/auth/components/forms/sign-up-form/sign-up-form.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-form',
   templateUrl: './sign-up-form.component.html',
-  styleUrls: ['./sign-up-form.component.sass']
+  styleUrls: ['./sign-up-form.component.sass'],
 })
 export class SignUpFormComponent {
+  constructor(private fb: FormBuilder) {}
 
+  signUpForm = this.fb.group({
+    emailAddress: ['', Validators.email],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    password: ['', Validators.minLength(8)],
+    confirmPassword: ['', Validators.required],
+  });
+
+  passwordsMatch(): boolean {
+    const password = this.signUpForm.value.password;
+    const confirmPassword = this.signUpForm.value.confirmPassword;
+    return password === confirmPassword;
+  }
+  onSubmit() {
+    if (this.signUpForm.valid && this.passwordsMatch()) {
+      const form = this.signUpForm.value;
+      alert(JSON.stringify(form));
+    } else {
+      alert('Please correct the form errors.');
+    }
+  }
 }
