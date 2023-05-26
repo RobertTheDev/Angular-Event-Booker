9d51bd66dcfc40d1b9db329ebe9976ecd12cd646
RobertTheDev <robertthedev@gmail.com>
2023-05-26 21:32:58 +0100

    Updated Styles To Sign Up View

--- src/app/auth/views/sign-up-view/sign-up-view.component.ts
+++ src/app/auth/views/sign-up-view/sign-up-view.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sign-up-view',
   templateUrl: './sign-up-view.component.html',
-  styleUrls: ['./sign-up-view.component.sass']
+  styleUrls: ['./sign-up-view.component.sass'],
 })
-export class SignUpViewComponent {
-
-}
+export class SignUpViewComponent {}
