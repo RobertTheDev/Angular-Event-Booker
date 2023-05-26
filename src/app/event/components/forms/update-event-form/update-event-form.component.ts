7c5ca2b5e3f9ed656b7431168f42f5f54825aff9
RobertTheDev <robertthedev@gmail.com>
2023-05-26 21:36:53 +0100

    Created Update Event Form Component

---
+++ src/app/event/components/forms/update-event-form/update-event-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-event-form',
+  templateUrl: './update-event-form.component.html',
+  styleUrls: ['./update-event-form.component.sass']
+})
+export class UpdateEventFormComponent {
+
+}
