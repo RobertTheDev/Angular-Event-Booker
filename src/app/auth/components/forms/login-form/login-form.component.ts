5ad88fffd35551566f6a6fcc77061f09551250c2
RobertTheDev <robertthedev@gmail.com>
2023-05-26 21:34:04 +0100

    Built Login Form

--- src/app/auth/components/forms/login-form/login-form.component.ts
+++ src/app/auth/components/forms/login-form/login-form.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.sass']
+  styleUrls: ['./login-form.component.sass'],
 })
 export class LoginFormComponent {
+  constructor(private fb: FormBuilder) {}
 
+  loginForm = this.fb.group({
+    emailAddress: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const form = this.loginForm.value;
+      alert(JSON.stringify(form));
+    } else {
+      alert('Please correct the form errors.');
+    }
+  }
 }
