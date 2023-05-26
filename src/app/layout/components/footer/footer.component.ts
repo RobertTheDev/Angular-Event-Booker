d7bf3c9a4fa8a534445a0d5a88fde005b9be64f1
RobertTheDev <robertthedev@gmail.com>
2023-05-26 18:18:36 +0100

    Added Footer Links

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import footerLinks from 'src/app/lib/links/footerLinks';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.sass']
+  styleUrls: ['./footer.component.sass'],
 })
 export class FooterComponent {
-
+  footerLinks = footerLinks;
 }
