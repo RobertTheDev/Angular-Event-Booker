e6cbb5c96159c13763e7acdb86c6e10c88daf741
RobertTheDev <robertthedev@gmail.com>
2023-05-26 07:43:10 +0100

    Created Auth Routes To Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { EventDetailComponent } from './event/components/details/event-detail/event-detail.component';
 import { EventCardListComponent } from './event/components/lists/event-card-list/event-card-list.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SignUpViewComponent } from './auth/views/sign-up-view/sign-up-view.component';
+import { LoginViewComponent } from './auth/views/login-view/login-view.component';
 
 const routes: Routes = [
   {
     path: '',
     component: EventCardListComponent,
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'sign-up',
+        component: SignUpViewComponent,
+      },
+      {
+        path: 'login',
+        component: LoginViewComponent,
+      },
+    ],
   },
   {
     path: 'event/:id',
