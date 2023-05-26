dd7f78a0131d73fa7eb9d06c16f358f3e07cc63d
RobertTheDev <robertthedev@gmail.com>
2023-05-26 18:18:23 +0100

    Added Header Links

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import headerLinks from '../../../lib/links/headerLinks';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.sass'],
 })
 export class HeaderComponent {
-
+  headerLinks = headerLinks;
 }
