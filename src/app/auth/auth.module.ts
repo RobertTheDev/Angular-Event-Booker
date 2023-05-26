63fd9e4a44f3de62dcaa04987fa25d02c47e2197
RobertTheDev <robertthedev@gmail.com>
2023-05-26 07:42:42 +0100

    Added Views To Auth Module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignUpViewComponent } from './views/sign-up-view/sign-up-view.component';
+import { LoginViewComponent } from './views/login-view/login-view.component';
+import { SignUpFormComponent } from './components/forms/sign-up-form/sign-up-form.component';
+import { LoginFormComponent } from './components/forms/login-form/login-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignUpViewComponent,
+    LoginViewComponent,
+    SignUpFormComponent,
+    LoginFormComponent,
+  ],
   imports: [CommonModule],
+  exports: [SignUpViewComponent, LoginViewComponent],
 })
 export class AuthModule {}
