86786677e200d52ded188a34b8a9f1b1cf800c11
RobertTheDev <robertthedev@gmail.com>
2023-05-28 10:23:38 +0100

    Updated Styling Of Header

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import headerLinks from '../../../lib/links/headerLinks';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,6 @@ import headerLinks from '../../../lib/links/headerLinks';
 })
 export class HeaderComponent {
   headerLinks = headerLinks;
+
+  faBars = faBars;
 }
