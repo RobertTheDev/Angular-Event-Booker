0e08f5db9074d839ed0d5aa21961c6875547ec11
RobertTheDev <robertthedev@gmail.com>
2023-05-28 10:24:21 +0100

    Added Font Awesome Module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
