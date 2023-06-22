21eb73be094bba8db105e647929a0fb061b03dbb
RobertTheDev <robertthedev@gmail.com>
2023-06-23 00:47:10 +0100

    Added Cookie Policy To info Module

--- src/app/info/info.module.ts
+++ src/app/info/info.module.ts
@@ -4,6 +4,7 @@ import { AboutViewComponent } from './views/about-view/about-view.component';
 import { PrivacyPolicyViewComponent } from './views/privacy-policy-view/privacy-policy-view.component';
 import { TermsViewComponent } from './views/terms-view/terms-view.component';
 import { AccessibilityStatementViewComponent } from './views/accessibility-statement-view/accessibility-statement-view.component';
+import { CookiePolicyViewComponent } from './views/cookie-policy-view/cookie-policy-view.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,8 @@ import { AccessibilityStatementViewComponent } from './views/accessibility-state
     PrivacyPolicyViewComponent,
     TermsViewComponent,
     AccessibilityStatementViewComponent,
+
+    CookiePolicyViewComponent,
   ],
   imports: [CommonModule],
   exports: [
