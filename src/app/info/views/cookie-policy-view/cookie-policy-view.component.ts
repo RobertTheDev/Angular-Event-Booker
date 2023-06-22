2308f04a991b84d2969c554a89c4ab3ca57b9d39
RobertTheDev <robertthedev@gmail.com>
2023-06-23 00:46:43 +0100

    Created Cookie Policy View

---
+++ src/app/info/views/cookie-policy-view/cookie-policy-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cookie-policy-view',
+  templateUrl: './cookie-policy-view.component.html',
+  styleUrls: ['./cookie-policy-view.component.sass']
+})
+export class CookiePolicyViewComponent {
+
+}
