df6fe9491d3440fa737f64f7a0a46f17c483b8d6
RobertTheDev <robertthedev@gmail.com>
2023-06-24 21:50:18 +0100

    Updated Event Type

--- src/app/interfaces/Event.ts
+++ src/app/interfaces/Event.ts
@@ -5,20 +5,52 @@ import Organisation from './Organisation';
 interface Event {
   id: string;
   createdAt: Date;
-  image: Image;
+  spacesBooked: number;
+  spacesTotal: number;
+  minimumAge: number;
+  bookingDeadlineDate: Date;
   name: string;
-  headline: string | null;
-  location: Location | null;
-  about: string | null;
-  date: Date;
-  refundPolicy: string | null;
+  headline: string;
+  refundPolicy: null;
+  facebook: string;
+  instagram: string;
+  twitter: string;
+  location: {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    address: string;
+    town: string;
+    country: string;
+  };
+  about: string;
+  ticketType: string;
+  image: {
+    createdAt: Date;
+    updatedAt: null;
+    url: string;
+    alt: string;
+  };
   price: {
     total: number;
     currencyUnit: string;
   };
-  ticketType: string;
-  duration: Duration;
-  organisation: Organisation | null;
+  startDate: Date;
+  endDate: Date;
+  tags: string[];
+  duration: {
+    length: number;
+    unit: string;
+  };
+  organisation: Organisation;
+  coverImage: {
+    url: string;
+    alt: string;
+    createdAt: Date;
+    updatedAt: null;
+  };
+  totalFollowers: number;
+  totalEvents: number;
 }
 
 export default Event;
