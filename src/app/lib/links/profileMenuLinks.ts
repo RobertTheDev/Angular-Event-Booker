8d91717172bf016851b959f50402b70dc2a009b5
RobertTheDev <robertthedev@gmail.com>
2023-06-24 16:20:33 +0100

    Added Profile Menu Links

---
+++ src/app/lib/links/profileMenuLinks.ts
@@ -0,0 +1,36 @@
+const profileMenuLinks: {
+  href: string;
+  name: string;
+}[] = [
+  {
+    href: '/',
+    name: 'My Profile',
+  },
+
+  {
+    href: '/',
+    name: 'Manage Events',
+  },
+  {
+    href: '/',
+    name: 'Tickets',
+  },
+  {
+    href: '/',
+    name: 'Following',
+  },
+  {
+    href: '/',
+    name: 'Edit Profile',
+  },
+  {
+    href: '/',
+    name: 'Account Settings',
+  },
+  {
+    href: '/',
+    name: 'Log Out',
+  },
+];
+
+export default profileMenuLinks;
