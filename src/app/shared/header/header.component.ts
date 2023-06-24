78e55ebf120453ed5d71b338e9f23f57ba66aba1
RobertTheDev <robertthedev@gmail.com>
2023-06-24 16:20:48 +0100

    Added Profile Menu Content

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import companyName from 'src/app/lib/constants/companyName';
 import headerLinks from 'src/app/lib/links/headerLinks';
+import profileMenuLinks from 'src/app/lib/links/profileMenuLinks';
 
 @Component({
   selector: 'app-header',
@@ -15,24 +16,29 @@ import headerLinks from 'src/app/lib/links/headerLinks';
 })
 export class HeaderComponent {
   headerLinks = headerLinks;
-
   companyName = companyName;
-
   faBell = faBell;
   faHeart = faHeart;
   faUser = faUser;
   faTicket = faTicket;
-
   controlNameActive: string | null = null;
-
-  profileMenu: boolean = false;
+  profileMenu = false;
+  signedIn = false;
+  isProfileMenuActive = false;
+  profileMenuLinks = profileMenuLinks;
 
   handleMouseOver(controlName: string) {
-    this.controlNameActive = controlName;
+    if (controlName === 'profileMenu') {
+      this.isProfileMenuActive = true;
+    } else {
+      this.controlNameActive = controlName;
+    }
   }
 
   handleMouseOut(controlName: string) {
-    if (this.controlNameActive === controlName) {
+    if (controlName === 'profileMenu') {
+      this.isProfileMenuActive = false;
+    } else if (this.controlNameActive === controlName) {
       this.controlNameActive = null;
     }
   }
@@ -43,5 +49,6 @@ export class HeaderComponent {
 
   closeProfileMenu() {
     this.profileMenu = false;
+    this.isProfileMenuActive = false;
   }
 }
