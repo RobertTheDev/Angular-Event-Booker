365d56a9baa05f97de32be79ccc6621c8faf2e4d
RobertTheDev <robertthedev@gmail.com>
2023-06-25 21:20:44 +0100

    Created Account Settings Links

---
+++ src/app/features/account-settings/account-settings-links.ts
@@ -0,0 +1,28 @@
+const accountSettingsLinks = [
+  {
+    href: '/account-settings/account-info',
+    name: 'Account Information',
+  },
+  {
+    href: '/account-settings/add-payment-card',
+    name: 'Add Payment Card',
+  },
+  {
+    href: '/change-email/',
+    name: 'Change Email',
+  },
+  {
+    href: '/change-password/',
+    name: 'Change Password',
+  },
+  {
+    href: '/close-account/',
+    name: 'Close Account',
+  },
+  {
+    href: '/linked-accounts/',
+    name: 'Linked Accounts',
+  },
+];
+
+export default accountSettingsLinks;
