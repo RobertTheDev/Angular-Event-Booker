78ba1d2245dd2426601e57de1e19100102609830
RobertTheDev <robertthedev@gmail.com>
2023-06-26 00:46:27 +0100

    Added Create Company Link

--- src/app/features/events/organise-event/organise-event.component.ts
+++ src/app/features/events/organise-event/organise-event.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-organise-event',
   templateUrl: './organise-event.component.html',
   styleUrls: ['./organise-event.component.sass'],
 })
-export class OrganiseEventComponent {}
+export class OrganiseEventComponent {
+  constructor(private router: Router) {}
+
+  navigateToLink(href: string) {
+    this.router.navigate([href]);
+  }
+}
