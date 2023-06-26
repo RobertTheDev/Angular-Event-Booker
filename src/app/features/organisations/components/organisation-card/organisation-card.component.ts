c994da93089122c3ac0784f8fe5a29239dcda8bd
RobertTheDev <robertthedev@gmail.com>
2023-06-26 13:26:11 +0100

    Created Organisation Card Component

---
+++ src/app/features/organisations/components/organisation-card/organisation-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organisation-card',
+  templateUrl: './organisation-card.component.html',
+  styleUrls: ['./organisation-card.component.sass']
+})
+export class OrganisationCardComponent {
+
+}
