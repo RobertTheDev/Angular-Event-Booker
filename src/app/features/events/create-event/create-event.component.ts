2c9ffc16156d7c213d38ed1e24bb4108119bc86b
RobertTheDev <robertthedev@gmail.com>
2023-06-26 10:20:52 +0100

    Added Character Limit To Title Input

--- src/app/features/events/create-event/create-event.component.ts
+++ src/app/features/events/create-event/create-event.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import eventCategories from 'src/app/lib/constants/eventCategories';
+import eventTypes from 'src/app/lib/constants/eventTypes';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
-  styleUrls: ['./create-event.component.sass']
+  styleUrls: ['./create-event.component.sass'],
 })
 export class CreateEventComponent {
+  eventCategories = eventCategories;
+  eventTypes = eventTypes;
 
+  inputText: string = '';
+  maxCharacterLimit: number = 75;
+
+  checkCharacterLimit() {
+    if (this.inputText.length > this.maxCharacterLimit) {
+      this.inputText = this.inputText.slice(0, this.maxCharacterLimit);
+    }
+  }
+
+  limitCharacterInput(event: KeyboardEvent) {
+    if (this.inputText.length >= this.maxCharacterLimit) {
+      event.preventDefault();
+    }
+  }
 }
