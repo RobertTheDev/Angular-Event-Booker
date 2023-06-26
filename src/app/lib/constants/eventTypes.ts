48d660fa6aeda8dcd1f21da9661a9b4e0614c9db
RobertTheDev <robertthedev@gmail.com>
2023-06-26 10:20:27 +0100

    Created Event Types Options

---
+++ src/app/lib/constants/eventTypes.ts
@@ -0,0 +1,24 @@
+const eventTypes = [
+  'Appearance or Signing',
+  'Attraction',
+  'Camp, Trip or Retreat',
+  'Concert or Performance',
+  'Conference',
+  'Convention',
+  'Course, Training or Workshop',
+  'Dinner or Gala',
+  'Festival or Fair',
+  'Game or Competition',
+  'Meeting or Network Event',
+  'Other',
+  'Party or Social Gathering',
+  'Race or Endurance Event',
+  'Rally',
+  'Screening',
+  'Seminar or Talk',
+  'Tour',
+  'Tournament',
+  'Tradeshow, Consumer Show or Expo',
+];
+
+export default eventTypes;
