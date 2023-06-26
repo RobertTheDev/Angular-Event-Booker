db77a451f5a7edf4d1696ec04e2df42e22e2d5d6
RobertTheDev <robertthedev@gmail.com>
2023-06-26 10:20:36 +0100

    Added Forms Module

--- src/app/features/events/events.module.ts
+++ src/app/features/events/events.module.ts
@@ -4,10 +4,17 @@ import { RouterModule } from '@angular/router';
 import { EventCardListComponent } from './event-card-list/event-card-list.component';
 import { EventCardComponent } from './event-card/event-card.component';
 import { EventsComponent } from './events/events.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [EventCardListComponent, EventCardComponent, EventsComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    EventCardListComponent,
+    EventCardComponent,
+    EventsComponent,
+    CreateEventComponent,
+  ],
+  imports: [CommonModule, RouterModule, FormsModule],
   exports: [EventCardComponent],
 })
 export class EventsModule {}
