5ee99534cccb8a2759a1163f6d16c41db45ae8db
RobertTheDev <robertthedev@gmail.com>
2023-06-26 13:26:31 +0100

    Added Card Components To Module

--- src/app/features/organisations/organisations.module.ts
+++ src/app/features/organisations/organisations.module.ts
@@ -5,6 +5,9 @@ import { EditOrganisationComponent } from './views/edit-organisation/edit-organi
 import { OrganisationComponent } from './views/organisation/organisation.component';
 import { OrganisationsComponent } from './views/organisations/organisations.component';
 import { MyOrganisationsComponent } from './views/my-organisations/my-organisations.component';
+import { SelectOrganisationCardComponent } from './components/select-organisation-card/select-organisation-card.component';
+import { ManageOrganisationCardComponent } from './components/manage-organisation-card/manage-organisation-card.component';
+import { OrganisationCardComponent } from './components/organisation-card/organisation-card.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,9 @@ import { MyOrganisationsComponent } from './views/my-organisations/my-organisati
     MyOrganisationsComponent,
     OrganisationComponent,
     OrganisationsComponent,
+    SelectOrganisationCardComponent,
+    ManageOrganisationCardComponent,
+    OrganisationCardComponent,
   ],
   imports: [CommonModule],
 })
