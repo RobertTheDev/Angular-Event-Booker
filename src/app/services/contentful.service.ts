cbcddadb5c1e266bc3facdb978f6147bef61c6b0
RobertTheDev <robertthedev@gmail.com>
2023-06-27 00:29:44 +0100

    Added New Query

--- src/app/services/contentful.service.ts
+++ src/app/services/contentful.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createClient } from 'contentful';
+import { from } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -14,6 +15,19 @@ export class ContentfulService {
   });
 
   getAllEntries() {
-    this.client.getEntries().then((entries) => console.log(entries));
+    const promise = this.client.getEntries();
+    return from(promise);
+  }
+
+  getEntryById(id: string) {
+    const promise = this.client.getEntry(id);
+    return from(promise);
+  }
+
+  getEntryByField() {
+    const promise = this.client.getEntries({
+      content_type: 'aboutPage',
+    });
+    return from(promise);
   }
 }
