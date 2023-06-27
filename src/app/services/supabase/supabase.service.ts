481d1b354376ec79ae6643b3e1eecde1aed98e7d
RobertTheDev <robertthedev@gmail.com>
2023-06-27 17:14:46 +0100

    Created Auth Functions In Supabase Service

--- src/app/services/supabase/supabase.service.ts
+++ src/app/services/supabase/supabase.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import {
+  AuthChangeEvent,
+  AuthSession,
+  Session,
+  SupabaseClient,
+  User,
+  createClient,
+} from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment';
+
+export interface IUser {
+  email: string;
+  name: string;
+  website: string;
+  url: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupabaseService {
+  private supabase: SupabaseClient;
+  _session: AuthSession | null = null;
 
-  constructor() { }
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+  }
+
+  public getUser() {
+    return this.supabase.auth.getUser();
+  }
+
+  get session() {
+    this.supabase.auth.getSession().then(({ data }) => {
+      this._session = data.session;
+    });
+    return this._session;
+  }
+
+  profile(user: User) {
+    return this.supabase
+      .from('profiles')
+      .select(`username, website, avatar_url`)
+      .eq('id', user.id)
+      .single();
+  }
+
+  authChanges(
+    callback: (event: AuthChangeEvent, session: Session | null) => void
+  ) {
+    return this.supabase.auth.onAuthStateChange(callback);
+  }
+
+  signIn(email: string) {
+    return this.supabase.auth.signInWithOtp({ email });
+  }
+
+  signOut() {
+    return this.supabase.auth.signOut();
+  }
 }
